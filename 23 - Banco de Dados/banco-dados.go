6b05aa1e31a02bdc4d1e50721aa4da1f101e61d7
Jamesson Junior <jamessonjr@gmail.com>
2026-06-16 23:52:47 -0300

    banco de dados

---
+++ 23 - Banco de Dados/banco-dados.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql" // o _ indica que esse pacote nao sera usado por esse codigo aqui e sim pelo database/sql
+)
+
+func main()  {
+	stringConexao := "my_user:my_password@/my_database?charset=utf8&parseTime=True&loc=Local"
+	db, erro := sql.Open("mysql", stringConexao)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	defer db.Close()
+	if erro = db.Ping(); erro != nil {
+		log.Fatal(erro)
+	}
+
+	fmt.Println("Conexão esta aberta!")
+
+	linhas, erro := db.Query("select * from usuarios")
+	if erro != nil {
+		log.Fatal(erro)
+	}
+	defer linhas.Close()
+	fmt.Println(linhas)
+
+}
